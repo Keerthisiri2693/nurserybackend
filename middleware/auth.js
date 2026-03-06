c06e4e04d8464f152e62a6a51a071665f48570b8
Keerthi Siri <keerthisiri2693@gmail.com>
2026-03-06 15:13:19 +0530

    mongodb connection fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,37 +1,25 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization;
+module.exports = function (req, res, next) {
+  const authHeader = req.headers.authorization;
 
-    console.log("Auth Header:", authHeader);
-
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized - Missing Bearer",
-      });
-    }
-
-    const token = authHeader.split(" ")[1];
-
-    console.log("Token:", token);
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    console.log("Decoded:", decoded);
-
-    req.user = {
-      id: decoded.id || decoded._id,
-    };
-
-    next();
-  } catch (error) {
-    console.log("JWT ERROR:", error.message);
-
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({
       success: false,
-      message: error.message,
+      message: "Unauthorized - Missing Bearer",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid Token",
     });
   }
 };
