f8c514ace5aad62207b72a2263e13f50bbf6a6e3
Keerthi Siri <keerthisiri2693@gmail.com>
2026-03-06 12:28:53 +0530

    mongodb connection fix

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000
+    });
+
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB connection error:", error.message);
-    // Do NOT stop the server
+    console.log("MongoDB connection error:", error.message);
   }
 };
 
