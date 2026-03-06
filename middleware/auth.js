07bcdc445624e5c64feb99001b53fa19adb179f7
Keerthi Siri <keerthisiri2693@gmail.com>
2026-03-06 14:57:05 +0530

    mongodb connection fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,27 +4,34 @@ module.exports = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
+    console.log("Auth Header:", authHeader);
+
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({
         success: false,
-        message: "Unauthorized",
+        message: "Unauthorized - Missing Bearer",
       });
     }
 
     const token = authHeader.split(" ")[1];
 
+    console.log("Token:", token);
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-console.log("Decoded:", decoded);
+
+    console.log("Decoded:", decoded);
+
     req.user = {
       id: decoded.id || decoded._id,
     };
 
     next();
-
   } catch (error) {
+    console.log("JWT ERROR:", error.message);
+
     return res.status(401).json({
       success: false,
-      message: "Unauthorized",
+      message: error.message,
     });
   }
 };
